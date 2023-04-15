1bec292c29432af9cf8846cbd1ee95a6c886f4d7
Clément Vandendaelen <vandendaelenclement@gmail.com>
2023-04-15 22:15:56 +0200

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Box, Container, Divider, Heading, Show, VStack } from '@chakra-ui/react';
+import { Container, Show } from '@chakra-ui/react';
 import Desktop from './Desktop';
 import Mobile from './Mobile';
 
